554695c9ce70e3d5e187d01496c0fc831b4ab255
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2018-07-24 14:00:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const Math = Math.floor(Math.random() * 100);
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -12,7 +12,7 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === 'Number') {
-    	message.reply(Math.floor(Math.random() * 100));
+    	message.reply(Math);
   	}
 });
   	
