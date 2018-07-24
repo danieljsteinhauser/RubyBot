944d8dc6aaaa9a1529ea1778d69fcf74575bf06c
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2018-07-24 16:21:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "Anna,";
+let prefix = 'Anna,';
 
 client.on('ready', () => {
     console.log('I am ready!');
