214fca16408e815bf030f722f04a22c7821504d6
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2018-07-24 15:29:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,25 @@ client.on('message', message => {
     if (message.content === 'Number') {
     	message.reply(Math.floor(Math.random() * 100));
   	}
-});
+})
+
+   if (message.substring(0, 1) == 'Anna,') {
+        const args = message.substring(1).split(' ');
+        const cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'hi':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Hello!'
+                });
+            break;
+            // Just add any case commands if you want to..
+}
+
+;
   	
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
