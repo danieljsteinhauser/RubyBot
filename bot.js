defe1321db2dd49d77971d10f0aef8aa66a0e084
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2018-07-25 00:13:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,14 @@ client.on("guildMemberAdd", (member) => {
   member.guild.channels.get("469920145335124008").send(`Thank you for joining the server "${member.user.username}"! Make sure to check out the list of roles you can choose from!`);
 });
 
-
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
 
 client.on('message', message => {
     
