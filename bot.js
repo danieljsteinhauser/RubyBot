8d1932893f4fe4f50dd31f25e8757478a100b9c1
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2018-07-25 00:16:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,13 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on("guildMemberAdd", (member) => {
-  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"`);
-  member.guild.channels.get("469920145335124008").send(`Thank you for joining the server "${member.user.username}"! Make sure to check out the list of roles you can choose from!`);
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
 });
 
 client.on('guildMemberAdd', member => {
