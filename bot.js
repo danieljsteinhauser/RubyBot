dd43527953d9d931963db2bbf45a3a463e8f45ed
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2022-01-18 11:31:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on('message', message => {
     	message.reply('\nSupporter - If you \'re a supporter of Fullmetal or Hayden, get this role to see even more of their content and let them know that you care!\nGranblue - Gives access to the Granblue Fantasy Versus related channels.\nSmash - Gives access to the Smash series related channels.\nBBTag - Gives access to the BlazBlue Cross Tag Battle related channels, where I reign supreme. \nGG - Gives access to the GGXrd2 channels.');
   	} else
                     
-if (message.content === 'goku') {
+if (message.content.startsWith(prefix + 'goku')) {
     message.member.roles.add(role);
     message.react('✅') 
 }
