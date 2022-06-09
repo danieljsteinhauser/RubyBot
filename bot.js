736158800227d6e74e51e4df5e9b63628a060f7e
FullmetalJrock <41394719+FullmetalJrock@users.noreply.github.com>
2022-06-09 17:51:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,16 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({
-const client = new Client({ 
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS //also enable in discord developer portal
-    ] 
-})
+const client = new Discord.Client();
 let prefix = '-';
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('guildMemberAdd', guildMember => {
-    guildMember.guild.channels.cache.get('928341780661960792').send(`**Welcome to the discord server, <@${guildMember.user.id}>!**`);
-});
+client.on('guildMemberAdd', member => {
+  console.log(member.user.username + 'welcome to the server!')
+    console.log(member)
+    const role = member.guild.roles.find('name', 'Guests');
+    member.addRole(role);
 });
 
 
@@ -31,28 +26,213 @@ client.on('message', message => {
     	message.channel.send(Math.floor(Math.random() * 100));
   	} else
         
+    if (message.content.startsWith(prefix + 'test')){
+        message.member.addRole('930603959209590796')
+        .then(console.log)
+        .catch(console.error);
+             let rudeRuby = Math.floor(Math.random() * 4);
+             switch(rudeRuby)
+        {
+            case 1 :
+                if(rudeRuby == 1){
+                message.reply('I\'m only doing this because I have to...');
+                };
+                break;
+                
+            case 2 :
+                if(rudeRuby == 2){
+                message.reply('can\'t you see I\'m busy?')
+                };
+                break;
+                
+            case 3 :
+                if(rudeRuby == 3){
+                message.reply('stop bothering me.')
+                };
+                break;
+                
+                default :
+                message.reply('you have your role, now go away...')
+        } 
+    } else
+            if (message.content.startsWith(prefix + 'Granblue')){
+        message.member.addRole('481485569063518217')
+        .then(console.log)
+        .catch(console.error);
+        let rudeRuby = Math.floor(Math.random() * 4);
+        switch(rudeRuby)
+        {
+            case 1 :
+                if(rudeRuby == 1){
+                message.reply('I\'m only doing this because I have to...');
+                };
+                break;
+                
+            case 2 :
+                if(rudeRuby == 2){
+                message.reply('can\'t you see I\'m busy?')
+                };
+                break;
+                
+            case 3 :
+                if(rudeRuby == 3){
+                message.reply('stop bothering me.')
+                };
+                break;
+                
+                default :
+                message.reply('you have your role, now go away...')
+        }
+    } else
+        
+         if (message.content.startsWith(prefix + 'Smash')){
+        message.member.addRole('481485523781943306')
+        .then(console.log)
+        .catch(console.error);
+             let rudeRuby = Math.floor(Math.random() * 4);
+             switch(rudeRuby)
+        {
+            case 1 :
+                if(rudeRuby == 1){
+                message.reply('I\'m only doing this because I have to...');
+                };
+                break;
+                
+            case 2 :
+                if(rudeRuby == 2){
+                message.reply('can\'t you see I\'m busy?')
+                };
+                break;
+                
+            case 3 :
+                if(rudeRuby == 3){
+                message.reply('stop bothering me.')
+                };
+                break;
+                
+                default :
+                message.reply('you have your role, now go away...')
+        }
+    } else
+        
+         if (message.content.startsWith(prefix + 'BBTag')){
+        message.member.addRole('481485486955692063')
+        .then(console.log)
+        .catch(console.error);
+             let rudeRuby = Math.floor(Math.random() * 4);
+             switch(rudeRuby)
+        {
+            case 1 :
+                if(rudeRuby == 1){
+                message.reply('I\'m only doing this because I have to...');
+                };
+                break;
+                
+            case 2 :
+                if(rudeRuby == 2){
+                message.reply('can\'t you see I\'m busy?')
+                };
+                break;
+                
+            case 3 :
+                if(rudeRuby == 3){
+                message.reply('stop bothering me.')
+                };
+                break;
+                
+                default :
+                message.reply('you have your role, now go away...')
+        }
+    } else
+        
+         if (message.content.startsWith(prefix + 'GG')){
+        message.member.addRole('481485431863771136')
+        .then(console.log)
+        .catch(console.error);
+             let rudeRuby = Math.floor(Math.random() * 4);
+             switch(rudeRuby)
+        {
+            case 1 :
+                if(rudeRuby == 1){
+                message.reply('I\'m only doing this because I have to...');
+                };
+                break;
+                
+            case 2 :
+                if(rudeRuby == 2){
+                message.reply('can\'t you see I\'m busy?')
+                };
+                break;
+                
+            case 3 :
+                if(rudeRuby == 3){
+                message.reply('stop bothering me.')
+                };
+                break;
+                
+                default :
+                message.reply('you have your role, now go away...')
+        }
+    } else
+        
+      if (message.content.startsWith(prefix + 'Supporter')){
+        message.member.addRole('481189063064551475')
+        .then(console.log)
+        .catch(console.error);
+          message.reply('you get to see more of me now, have fun :smiling_imp:');
+    } else
+        
           if (message.content.startsWith(prefix + 'roles')) {
     	message.reply('\nSupporter - If you \'re a supporter of Fullmetal or Hayden, get this role to see even more of their content and let them know that you care!\nGranblue - Gives access to the Granblue Fantasy Versus related channels.\nSmash - Gives access to the Smash series related channels.\nBBTag - Gives access to the BlazBlue Cross Tag Battle related channels, where I reign supreme. \nGG - Gives access to the GGXrd2 channels.');
   	} else
         
-        if(message.content.startsWith(prefix + 'strive')){
+                   if (message.content.startsWith(prefix + 'remove test')) {
+                        message.member.removeRole('930603959209590796')
+  .then(console.log)
+  .catch(console.error);
+                         message.reply('you\'ve been successfully removed')
+                    } else
+                        
+                        if (message.content.startsWith(prefix + 'remove Granblue')) {
+                        message.member.removeRole('481485569063518217')
+  .then(console.log)
+  .catch(console.error);
+                         message.reply('you\'ve been successfully removed')
+                    } else
+                    
             
-            let role = message.member.guild.roles.cache.find(role => role.name === "Strive");
-            
-if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
-        
-        
-        }
-                      
-                          
-});
-
-
- 
-       
-
-
-
+              if (message.content.startsWith(prefix + 'remove Smash')){
+                    message.member.removeRole('481485523781943306')
+  .then(console.log)
+  .catch(console.error);
+                   message.reply('you\'ve been successfully removed')
+              } else
+                    
+                    
+       if (message.content.startsWith(prefix + 'remove Supporter')){
+          message.member.removeRole('481189063064551475')
+  .then(console.log)
+  .catch(console.error);
+            message.reply('you\'ve been successfully removed')
+                      } else
+                    
+                    
+                       if (message.content.startsWith(prefix + 'remove BBTag')){
+                    message.member.removeRole('481485486955692063')
+  .then(console.log)
+  .catch(console.error);
+                           message.reply('I don\'t blame you for getting salty, losing to me all the time. You\'re always welcome to come back and repeat the process though.')
+                       } else
+                    
+                    
+                     
+                    if (message.content.startsWith(prefix + 'remove GG')){
+                    message.member.removeRole('481485431863771136')
+  .then(console.log)
+  .catch(console.error);
+                        message.reply('you\'ve been successfully removed')
+                    }
+                                
 });
   	
 // THIS  MUST  BE  THIS  WAY
