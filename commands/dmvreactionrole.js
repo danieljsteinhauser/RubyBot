cd7d3e81168f897b2f7c0b83be18277f023aba3e
Daniel Steinhauser <59288768+danieljsteinhauser@users.noreply.github.com>
2022-08-24 22:24:09 -0400

    Add files via upload

--- commands/dmvreactionrole.js
+++ commands/dmvreactionrole.js
@@ -6,19 +6,14 @@ module.exports= {
      async execute(message, args, Discord, client){
 
 //////////////////////Channel and Role Info////////////////////////
-          const channel ='996036666017988708' // Laika's Bot Testing Channel
-          // const channel = '871852170188832778' // DMV role-assign
-          // const channel = '928335650225270784' // DMV Bot Testing Channel
-
-
-          const marylandRole = message.guild.roles.cache.find(role => role.name === "MD") // Use Role's Name
-          const virginiaRole = message.guild.roles.cache.find(role => role.name === "VA")
-          const dcRole = message.guild.roles.cache.find(role => role.name === "DC")
+          //const channel ='996036666017988708'; // Laika's Bot Testing Channel
+           const channel = '871852170188832778' // DMV role-assign
+          // const channel = '928335650225270784' // Bot Testing Channel
 
  //////////////Emojis///////////////// 
-          const mdFlag = message.guild.emojis.cache.find(emoji => emoji.name === 'MDFlag') // pulls image from server's list, works based on name of emoji
-          const vaFlag = message.guild.emojis.cache.find(emoji => emoji.name === 'VAFlag')
-          const dcFlag = message.guild.emojis.cache.find(emoji => emoji.name === 'DCFlag')
+          const mdFlag = message.guild.emojis.cache.find(emoji => emoji.name === 'MDFlag'); // pulls image from server's list, works based on name of emoji
+          const vaFlag = message.guild.emojis.cache.find(emoji => emoji.name === 'VAFlag');
+          const dcFlag = message.guild.emojis.cache.find(emoji => emoji.name === 'DCFlag');
 
 /////////////////Embed//////////////////////
           let embed = new Discord.MessageEmbed()
@@ -34,50 +29,4 @@ module.exports= {
           messageEmbed.react(mdFlag)
                .then(() => messageEmbed.react(vaFlag)) // then method ensures bot reacts to the embed in the correct order.
                .then(() => messageEmbed.react(dcFlag))
-          
-///////////////Reaction Add Protocols and Safety Checks ///////////////// 
-          client.on('messageReactionAdd', async (reaction, user) => {
-               if (reaction.partial) await reaction.fetch() 
-               if (user.bot) return //skips bot reactions
-               if (!reaction.message.guild) return //reaction has to be from server member
-               
-               if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji === mdFlag){
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(marylandRole) // If Permission Error Happens, Make sure Bot is in Bot Role with Permissions
-               }}
-               if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji === vaFlag){
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(virginiaRole) 
-               }}
-               if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji === dcFlag){
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(dcRole) 
-               }}          
-          else {
-               return
-          }})
-
- ///////////////Reaction Remove Protocols and Safety Checks /////////////////     
-          client.on('messageReactionRemove', async (reaction, user) => {
-               if (reaction.partial) await reaction.fetch()
-               if (user.bot) return
-               if (!reaction.message.guild) return
-
-               if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji === mdFlag){
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(marylandRole) // If Permission Error Happens, Make sure Bot is in Bot Role with Permissions
-               }}
-               if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji === vaFlag){
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(virginiaRole) 
-               }}
-               if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji === dcFlag){
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(dcRole) 
-               }}
-               else {
-                    return
-          }
-
-     })
 }}
